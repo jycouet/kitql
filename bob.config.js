b147b64c147a0b6e17ee45281ecd550a2a745ab5
jycouet <jycouet@gmail.com>
2022-01-26 00:10:50 +0100

    :art: NEW: config files

---
+++ bob.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  scope: "@kitql", // Scope of organization
+  ignore: ["@kitql/client"], // ignored packages
+  track: [
+    // files in root that mark the entire workspace as dirty
+    "bob.config.js", // we could include it in Bob itself but we decided to turn your life into hell :)
+    "jest.config.js",
+    "jest-project.js",
+    "package.json",
+    "tsconfig.json",
+    // files in packages that mark the package as dirty
+    "<project>/src/**",
+    "<project>/jest.config.js",
+    "<project>/package.json",
+    "<project>/tsconfig.json",
+  ],
+  base: "origin/master", // we need to compare against something
+  commands: {
+    test: {
+      track: ["<project>/tests/**"],
+      run(affected) {
+        // {
+        //   paths: string[] <- ['packages/core', 'packages/cli']
+        //   names: string[] <- ['@foo/core', '@foo/cli']
+        // }
+
+        // why such a weird syntax? We use spawn, so you have too
+        return [`yarn`, ["test", ...affected.paths]];
+      },
+    },
+    build: {
+      run() {
+        return [`yarn`, ["build"]];
+      },
+    },
+  },
+};
---
+++ jest.project.js
@@ -0,0 +1,33 @@
+const { resolve } = require("path");
+const { pathsToModuleNameMapper } = require("ts-jest");
+
+const ROOT_DIR = __dirname;
+const TSCONFIG = resolve(ROOT_DIR, "tsconfig.json");
+const tsconfig = require(TSCONFIG);
+const CI = !!process.env.CI;
+
+module.exports = ({ dirname, projectMode = true }) => {
+  const pkg = require(resolve(dirname, "package.json"));
+
+  return {
+    ...(CI || !projectMode
+      ? {}
+      : { displayName: pkg.name.replace("@graphql-codegen/", "") }),
+    transform: { "^.+\\.tsx?$": "babel-jest" },
+    testEnvironment: "node",
+    rootDir: dirname,
+    restoreMocks: true,
+    reporters: ["default"],
+    modulePathIgnorePatterns: ["dist"],
+    moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+      prefix: `${ROOT_DIR}/`,
+    }),
+    cacheDirectory: resolve(
+      ROOT_DIR,
+      `${CI ? "" : "node_modules/"}.cache/jest`
+    ),
+    // setupFiles: [`${ROOT_DIR}/dev-test/setup.js`],
+    collectCoverage: false,
+    testTimeout: 20000,
+  };
+};
