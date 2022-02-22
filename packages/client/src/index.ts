7d544110a7401bad8ac769091a1a9b1fa872dd27
jycouet <jycouet@gmail.com>
2022-02-22 10:45:32 +0100

    :bug: FIX: entry pck for bob has to be ./src/index

---
+++ packages/client/src/index.ts
@@ -0,0 +1,3 @@
+export { isLoading } from './lib/toExport';
+export { defaultStoreValue, KitQLClient, RequestFrom, RequestStatus } from './lib/toExport';
+export type { RequestParameters, RequestResult } from './lib/toExport';
