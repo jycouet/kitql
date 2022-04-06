70b8421979c306b2f16d9bfdc74321245708f778
jycouet <jycouet@gmail.com>
2022-04-07 00:48:55 +0200

    :sparkles: DOC: add comments

--- examples/demo1/src/routes/continent/index.svelte
+++ examples/demo1/src/routes/continent/index.svelte
@@ -4,11 +4,17 @@
 	import { KitQLInfo } from '@kitql/all-in';
 
 	export async function load({ fetch }) {
-		await KQL_AllContinents.queryLoad({ fetch });
+		await KQL_AllContinents.queryLoad({ fetch }); // Work in SSR & Client mode
+		// await KQL_another-operation-filling-another-store.queryLoad({ fetch });
 		return {};
 	}
 </script>
 
+<!-- 👇 Store with data, status, ... -->
+<!-- {JSON.stringify($KQL_AllContinents.data?.continents, null, 2)} -->
+
+<!-- 👇 Widget to help you building your app -->
 <KitQLInfo store={KQL_AllContinents} />
 
+<!-- 👇 Store can be used inside a component directly -->
 <Continents />
