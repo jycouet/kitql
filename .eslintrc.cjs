1fbde23c4211725e10aa623ab2fa4afd2b269071
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-05-17 18:00:56 +0200

    adjust root eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,13 @@
 module.exports = {
+	root: true,
 	extends: '@theguild',
-	rules: {},
-	ignorePatterns: ['examples', 'website']
+	rules: {
+		'unicorn/no-array-push-push': 'off'
+	},
+	ignorePatterns: ['examples', 'website'],
+	plugins: ['svelte3'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	settings: {
+		'svelte3/typescript': () => require('typescript')
+	},
 };
