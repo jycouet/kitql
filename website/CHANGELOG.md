d10f2c130d0ef552dc5fc97b1b7c23d6b06ab6a5
jycouet <jycouet@gmail.com>
2022-09-14 23:39:21 +0200

    :construction: UPDATE: website

---
+++ website/CHANGELOG.md
@@ -0,0 +1,37 @@
+# @kitql/website
+
+## 0.0.2
+
+### Patch Changes
+
+- [`cef5c74`](https://github.com/jycouet/kitql/commit/cef5c741d70d403bbb840e61afedf958586a2307) Thanks [@jycouet](https://github.com/jycouet)! - bump codegen tools
+
+## 0.0.1
+
+### Patch Changes
+
+- 55b0742: bump
+
+## 0.2.1
+
+### Patch Changes
+
+- 887591f: doc update & init lib (optional)
+
+## 0.2.0
+
+### Minor Changes
+
+- ed558f9: refactoring to remove comp library - KitQLInfo is in All-In directly
+
+## 0.1.0
+
+### Minor Changes
+
+- f8a035e: align version number
+
+## 0.0.1
+
+### Patch Changes
+
+- a692114: fix xPath
---
+++ website/next.config.mjs
@@ -0,0 +1,24 @@
+import { createRequire } from 'node:module'
+import nextBundleAnalyzer from '@next/bundle-analyzer'
+import { withGuildDocs } from '@guild-docs/server'
+import { register } from 'esbuild-register/dist/node.js'
+import { i18n } from './next-i18next.config.js'
+
+register({ extensions: ['.ts', '.tsx'] })
+
+const require = createRequire(import.meta.url)
+
+const { getRoutes } = require('./routes.ts')
+
+const withBundleAnalyzer = nextBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+export default withBundleAnalyzer(
+  withGuildDocs({
+    i18n,
+    getRoutes,
+    redirects: () => {
+      return []
+    },
+  })
+)
