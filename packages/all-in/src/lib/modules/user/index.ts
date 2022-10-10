f57f98a72cc05f5c1c95b7156d04657df5f9bd3a
jycouet <jycouet@gmail.com>
2022-10-10 22:30:33 +0200

    :children_crossing: FIX: generated path

--- packages/all-in/src/lib/modules/user/index.ts
+++ packages/all-in/src/lib/modules/user/index.ts
@@ -1,8 +1,8 @@
 import { createModule } from 'graphql-modules'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
-import { resolvers } from './_kitql/resolvers'
-import { typeDefs } from './_kitql/typedefs'
+import { resolvers } from './$kitql/resolvers'
+import { typeDefs } from './$kitql/typedefs'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
