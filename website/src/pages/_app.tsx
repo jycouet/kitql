b5e7ec931724c547cf997dd3921529812f42e6a0
jycouet <jycouet@gmail.com>
2022-10-11 18:10:09 +0200

    :bug: FIX: rmv unused code

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps } from 'next/app';
-import '@theguild/components/style.css';
+import { AppProps } from 'next/app'
+import '@theguild/components/style.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -1,6 +1,6 @@
-const config = require('@theguild/tailwind-config');
+const config = require('@theguild/tailwind-config')
 
 module.exports = {
   ...config,
   safelist: ['dark:invert'], // because `dark:invert` not in tsx file but in ts
-};
+}
