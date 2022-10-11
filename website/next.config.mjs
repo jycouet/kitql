f77b722eb997c5e72784609fd42f7de1dbef129a
jycouet <jycouet@gmail.com>
2022-10-11 20:20:55 +0200

    :art: IMPROVE: look & feel

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,13 +1,13 @@
-import { withGuildDocs } from '@theguild/components/next.config';
+import { withGuildDocs } from '@theguild/components/next.config'
 
 export default withGuildDocs({
-	images: {
-		unoptimized: true
-	},
-	redirects: () =>
-		Object.entries({}).map(([from, to]) => ({
-			source: from,
-			destination: to,
-			permanent: true
-		}))
-});
+  images: {
+    unoptimized: true,
+  },
+  redirects: () =>
+    Object.entries({}).map(([from, to]) => ({
+      source: from,
+      destination: to,
+      permanent: true,
+    })),
+})
