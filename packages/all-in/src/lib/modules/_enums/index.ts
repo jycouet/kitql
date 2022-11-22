e5e3d92a8c7bc879eb737f202202b6e48321368e
JYC <jycouet@gmail.com>
2022-11-22 21:31:38 +0100

    :arrow_up: UPDATE: dep (#275)

--- packages/all-in/src/lib/modules/_enums/index.ts
+++ packages/all-in/src/lib/modules/_enums/index.ts
@@ -2,7 +2,7 @@ import { createModule } from 'graphql-modules'
 
 import { typeDefs } from './$kitql/typedefs'
 
-export const _enumsModule =  createModule({
+export const _enumsModule = createModule({
 	id: 'enums-module',
 	typeDefs
 })
