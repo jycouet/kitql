083530a5b43bc9e4f9542483098e8e26bac53a5a
jycouet <jycouet@gmail.com>
2023-04-27 17:42:55 +0200

    :bug: FIX: pnpm

--- packages/handles/.eslintrc.cjs
+++ packages/handles/.eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'plugin:svelte3/recommended', 'prettier'],
+	extends: ['eslint:recommended', 'prettier'],
+	plugins: ['svelte3'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
