9dd54648b24407ece9780f4d1e505c3c93b860ec
jycouet <jycouet@gmail.com>
2023-10-13 21:24:51 +0200

    :construction: UPDATE: preparing things

--- scripts/package.js
+++ scripts/package.js
@@ -13,6 +13,46 @@ const packageJsonDistPath = path.join(packageDirPath, distFolder, 'package.json'
 // read file into JSON
 const pkg = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'))
 
+// rewrite package.json te be in the right order!
+// fs.writeFileSync(
+//   packageJsonPath,
+//   JSON.stringify(
+//     {
+//       name: pkg.name,
+//       description: pkg.description ?? 'Missing!!!',
+//       version: pkg.version,
+//       license: 'MIT',
+//       type: 'module',
+//       repository: {
+//         type: pkg.repository?.type ?? 'git',
+//         url: pkg.repository?.url ?? 'https://github.com/jycouet/kitql',
+//         directory: pkg.repository?.directory ?? 'packages/' + pkg.name,
+//         homepage:
+//           pkg.repository?.homepage ?? 'https://github.com/jycouet/kitql/tree/main/packages/???',
+//       },
+//       scripts: pkg.scripts,
+//       peerDependencies: pkg.peerDependencies,
+//       devDependencies: pkg.devDependencies,
+//       dependencies: pkg.dependencies,
+//       publishConfig: {
+//         directory: pkg.publishConfig?.directory ?? 'dist',
+//         access: pkg.publishConfig?.access ?? 'public',
+//       },
+//       files: pkg.files ?? ['dist', '!dist/**/*.test.*', '!dist/**/*.spec.*'],
+//       svelte: pkg.svelte ?? './dist/index.js',
+//       types: pkg.types ?? './dist/index.d.ts',
+//       exports: {
+//         '.': {
+//           types: pkg.types ?? './dist/index.d.ts',
+//           svelte: pkg.svelte ?? './dist/index.js',
+//         },
+//       },
+//     },
+//     null,
+//     2,
+//   ),
+// )
+
 // adjust pkg json however you like ...
 delete pkg.publishConfig
 delete pkg.scripts
