9b66503a1031d05a61a9748c73f4f293c0766695
jycouet <jycouet@gmail.com>
2023-10-13 18:40:02 +0200

    :children_crossing: UPDATE: with 2 imports

--- packages/helper/src/routes/about/+page.ts
+++ packages/helper/src/routes/about/+page.ts
@@ -1,10 +1,10 @@
+import { bgYellowBright, underline } from '$lib/colors/index.js'
+// import { bgYellowBright, underline } from '$lib/index.js'
 import { Log } from '$lib/Log.js'
 
-import type { PageLoad } from './$types'
-
-export const load = (async () => {
+export const load = async () => {
   const l = new Log('@kitql/helper')
-  l.info('load!')
+  l.info(underline(bgYellowBright('load!')))
 
   return {}
-}) satisfies PageLoad
+}
