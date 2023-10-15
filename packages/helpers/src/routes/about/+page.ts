5a2e77faca3aed7552967d6f017b9f1de4550ea4
jycouet <jycouet@gmail.com>
2023-10-15 09:01:43 +0200

    :sparkles: UPDATE: rename helper to helpers

--- packages/helpers/src/routes/about/+page.ts
+++ packages/helpers/src/routes/about/+page.ts
@@ -3,7 +3,7 @@ import { stry0, underline } from '$lib/index.js'
 import { Log } from '$lib/Log.js'
 
 export const load = async () => {
-  const l = new Log('@kitql/helper')
+  const l = new Log('@kitql/helpers')
   l.info(underline('load!'))
   return {}
 }
