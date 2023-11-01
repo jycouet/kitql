14923e5f9b203064e0c615d44ebc2f6d81bce3cf
jycouet <jycouet@gmail.com>
2023-11-01 10:25:23 +0200

    :tada: NEW: watch can look at an array of globs now

---
+++ .changeset/eleven-lies-behave.md
@@ -0,0 +1,5 @@
+---
+'vite-plugin-watch-and-run': patch
+---
+
+watch can look at an array of globs now
---
+++ packages/vite-plugin-watch-and-run/src/lib/micromatch.spec.ts
@@ -0,0 +1,59 @@
+// micromatch.isMatch(absolutePath, info.watch)
+
+import micromatch from 'micromatch'
+import { describe, expect, it } from 'vitest'
+
+describe('micromatch', () => {
+  it('1', async () => {
+    expect(
+      micromatch.isMatch(
+        '/home/asbPath/site/[id]/one/[hello]/+page.server.ts',
+        '**/+page.server.ts',
+      ),
+    ).toBe(true)
+  })
+
+  it('2', async () => {
+    expect(
+      micromatch.isMatch(
+        '/home/asbPath/site/[id]/one/[hello]/+page.server.ts',
+        '**/(+page.server.ts)',
+      ),
+    ).toBe(true)
+  })
+
+  it('3', async () => {
+    expect(
+      micromatch.isMatch('/home/asbPath/site/[id]/one/[hello]/+page.server.ts', [
+        '**/+page.server.ts',
+        '**/+page.svelte',
+      ]),
+    ).toBe(true)
+  })
+
+  it('4', async () => {
+    expect(
+      micromatch.isMatch('/home/asbPath/site/[id]/one/[hello]/+page.server.ts', [
+        '**/+page.svelte',
+      ]),
+    ).toBe(false)
+  })
+
+  it('5', async () => {
+    expect(
+      micromatch.isMatch('/home/asbPath/site/[id]/one/[hello]/+page.svelte', [
+        '**/+page.server.ts',
+        '**/+page.svelte',
+      ]),
+    ).toBe(true)
+  })
+
+  it('5', async () => {
+    expect(
+      micromatch.isMatch('/home/asbPath/site/[id]/one/[hello]/+page.svelteNOT', [
+        '**/+page.server.ts',
+        '**/+page.svelte',
+      ]),
+    ).toBe(false)
+  })
+})
