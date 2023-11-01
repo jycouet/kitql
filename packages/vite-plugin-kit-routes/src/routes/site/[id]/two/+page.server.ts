8139783cebd5520920fc4de909274b0bace12f20
jycouet <jycouet@gmail.com>
2023-11-01 12:31:21 +0200

    :tada: NEW: parsing files

--- packages/vite-plugin-kit-routes/src/routes/site/[id]/two/+page.server.ts
+++ packages/vite-plugin-kit-routes/src/routes/site/[id]/two/+page.server.ts
@@ -4,10 +4,4 @@ export const actions = {
   default: async event => {
     // TODO log the user in
   },
-  login: async event => {
-    // TODO log the user in
-  },
-  register: async event => {
-    // TODO register the user
-  },
 } satisfies Actions
