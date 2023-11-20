c8d56c8cbee06e93fe204c5975a2bdda60a0778d
jycouet <jycouet@gmail.com>
2023-11-20 22:11:53 +0100

    :white_check_mark: NEW: check for publish package

--- scripts/packageJsonFormat.js
+++ scripts/packageJsonFormat.js
@@ -20,7 +20,7 @@ fs.writeFileSync(
       license: 'MIT',
       type: 'module',
       repository: {
-        type: pkg.repository?.type ?? 'git',
+        type: 'git',
         url: pkg.repository?.url ?? 'https://github.com/jycouet/kitql',
         directory: pkg.repository?.directory ?? 'packages/' + pkg.name,
         homepage:
@@ -32,18 +32,18 @@ fs.writeFileSync(
       dependencies: pkg.dependencies,
       sideEffects: pkg.sideEffects ?? false, // Key for  tree shaking!
       publishConfig: {
-        directory: pkg.publishConfig?.directory ?? 'dist',
-        access: pkg.publishConfig?.access ?? 'public',
+        directory: 'dist',
+        access: 'public',
       },
-      files: pkg.files ?? ['dist', '!dist/**/*.test.*', '!dist/**/*.spec.*'],
-      svelte: pkg.svelte ?? './esm/index.js',
-      types: pkg.types ?? './esm/index.d.ts',
+      files: ['dist', '!dist/**/*.test.*', '!dist/**/*.spec.*'],
+      svelte: './esm/index.js',
+      types: './esm/index.d.ts',
       exports: {
         '.': {
-          require: pkg.exports.require ?? './cjs/index.js',
-          types: pkg.exports.types ?? './esm/index.d.ts',
-          default: pkg.exports.default ?? './esm/index.js',
-          svelte: pkg.exports.svelte ?? './esm/index.js',
+          require: './cjs/index.js',
+          types: './esm/index.d.ts',
+          default: './esm/index.js',
+          svelte: './esm/index.js',
         },
       },
     },
