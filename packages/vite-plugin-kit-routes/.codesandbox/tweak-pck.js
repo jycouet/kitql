7b6d786da92f77ec5130e9e49869c72449a57f35
jycouet <jycouet@gmail.com>
2023-11-23 18:00:51 +0100

    :fire: REMOVE: some obsolete files

--- packages/vite-plugin-kit-routes/.codesandbox/tweak-pck.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs'
-
-const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'))
-delete pkg.devDependencies['eslint-config-kitql']
-pkg.dependencies['@kitql/helpers'] = '*'
-pkg.dependencies['vite-plugin-watch-and-run'] = '*'
-fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2))
