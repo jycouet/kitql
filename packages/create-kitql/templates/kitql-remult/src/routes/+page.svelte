ef380ee61e8e8c473b84f605c6675c3698122e04
jycouet <jycouet@gmail.com>
2023-11-24 22:33:00 +0100

    okay first draft

--- packages/create-kitql/templates/kitql-remult/src/routes/+page.svelte
+++ packages/create-kitql/templates/kitql-remult/src/routes/+page.svelte
@@ -1,19 +1,56 @@
 <script lang="ts">
 	import { remult } from 'remult';
-	import { onMount } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	import { Task } from '../shared';
 
 	let list: Task[] = [];
+	let unSub: (() => void) | null = null;
+
+	let title = '';
 
 	onMount(async () => {
-		list = await remult.repo(Task).find();
+		unSub = remult
+			.repo(Task)
+			.liveQuery()
+			.subscribe((info) => {
+				list = info.applyChanges(list);
+			});
 	});
+
+	onDestroy(() => {
+		unSub && unSub();
+	});
+
+	const add = async () => {
+		try {
+			await remult.repo(Task).insert({ title });
+			title = '';
+		} catch (error) {
+			alert((error as { message: string }).message);
+		}
+	};
+
+	const update = async (task: Task) => {
+		await remult.repo(Task).save({ ...task, completed: !task.completed });
+	};
 </script>
 
 <h2>Home</h2>
 
 <ul>
 	{#each list as task}
-		<li>{task.title}</li>
+		<li>
+			<button
+				style={task.completed ? 'text-decoration: line-through;color:green' : ''}
+				on:click={() => update(task)}
+			>
+				{task.title}
+			</button>
+		</li>
 	{/each}
 </ul>
+
+<form on:submit|preventDefault={add}>
+	<input type="text" bind:value={title} />
+	<button type="submit">Add</button>
+</form>
--- packages/create-kitql/templates/kitql-remult/src/shared/Task.ts
+++ packages/create-kitql/templates/kitql-remult/src/shared/Task.ts
@@ -7,7 +7,11 @@ export class Task {
 	@Fields.cuid()
 	id!: string;
 
-	@Fields.string()
+	@Fields.string<Task>({
+		validate: (task) => {
+			if (task.title.length < 3) throw 'The title must be at least 3 characters long';
+		}
+	})
 	title: string = '';
 
 	@Fields.boolean()
