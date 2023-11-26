868bd8e80c10084a3287e48068f04adbb705abfa
jycouet <jycouet@gmail.com>
2023-11-26 14:58:12 +0100

    :boom: NEW: action "default" needs to be specified

--- packages/vite-plugin-kit-routes/src/routes/[[lang]]/contract/[id]/+page.svelte
+++ packages/vite-plugin-kit-routes/src/routes/[[lang]]/contract/[id]/+page.svelte
@@ -7,7 +7,7 @@
 
   const id = $page.params.id
 
-  const action = ACTIONS.lang_contract_id({
+  const action = ACTIONS.lang_contract_id('default', {
     lang: $page.params.lang,
     id,
   })
--- packages/vite-plugin-kit-routes/vite.config.ts
+++ packages/vite-plugin-kit-routes/vite.config.ts
@@ -12,83 +12,83 @@ export default defineConfig({
   plugins: [
     sveltekit(),
     // demo
-    // kitRoutes<KIT_ROUTES>({
-    //   format: '_',
-    //   logs: ['update', 'errors'],
-    //   // path_base: true,
-    //   // default_type: 'string',
-    //   // extra_search_params: 'with',
-    //   // generated_file_path: 'src/lib/another_path_for_the_file.ts',
-    //   post_update_run: 'npm exec prettier ./src/lib/ROUTES.ts -- -w',
+    kitRoutes<KIT_ROUTES>({
+      format: '_',
+      logs: ['update', 'errors'],
+      // path_base: true,
+      // default_type: 'string',
+      // extra_search_params: 'with',
+      // generated_file_path: 'src/lib/another_path_for_the_file.ts',
+      post_update_run: 'npm exec prettier ./src/lib/ROUTES.ts -- -w',
 
-    //   PAGES: {
-    //     subGroup2: {
-    //       explicit_search_params: {
-    //         first: {
-    //           required: true,
-    //         },
-    //       },
-    //     },
-    //     lang_site: {
-    //       extra_search_params: 'with',
-    //       explicit_search_params: { limit: { type: 'number' } },
-    //       params: {
-    //         // yop: { type: 'number' },
-    //       },
-    //     },
-    //     lang_site_id: {
-    //       explicit_search_params: { limit: { type: 'number' }, demo: { type: 'string' } },
-    //       params: {
-    //         id: { type: 'string', default: '"Vienna"' },
-    //         lang: { type: "'fr' | 'hu' | undefined", default: '"fr"' },
-    //       },
-    //     },
-    //     lang_site_contract_siteId_contractId: {
-    //       explicit_search_params: { limit: { type: 'number' } },
-    //     },
-    //   },
-    //   SERVERS: {
-    //     // site: {
-    //     //   params: { }
-    //     // }
-    //     // yop: {},
-    //   },
-    //   ACTIONS: {
-    //     lang_contract_id: {
-    //       explicit_search_params: {
-    //         limit: { type: 'number' },
-    //       },
-    //     },
-    //     lang_site_contract_siteId_contractId: {
-    //       explicit_search_params: {
-    //         extra: { type: "'A' | 'B'", default: '"A"' },
-    //       },
-    //     },
-    //   },
-    //   LINKS: {
-    //     // reference to a hardcoded link
-    //     twitter: 'https://twitter.com/jycouet',
+      PAGES: {
+        subGroup2: {
+          explicit_search_params: {
+            first: {
+              required: true,
+            },
+          },
+        },
+        lang_site: {
+          extra_search_params: 'with',
+          explicit_search_params: { limit: { type: 'number' } },
+          params: {
+            // yop: { type: 'number' },
+          },
+        },
+        lang_site_id: {
+          explicit_search_params: { limit: { type: 'number' }, demo: { type: 'string' } },
+          params: {
+            id: { type: 'string', default: '"Vienna"' },
+            lang: { type: "'fr' | 'hu' | undefined", default: '"fr"' },
+          },
+        },
+        lang_site_contract_siteId_contractId: {
+          explicit_search_params: { limit: { type: 'number' } },
+        },
+      },
+      SERVERS: {
+        // site: {
+        //   params: { }
+        // }
+        // yop: {},
+      },
+      ACTIONS: {
+        lang_contract_id: {
+          explicit_search_params: {
+            limit: { type: 'number' },
+          },
+        },
+        lang_site_contract_siteId_contractId: {
+          explicit_search_params: {
+            extra: { type: "'A' | 'B'", default: '"A"' },
+          },
+        },
+      },
+      LINKS: {
+        // reference to a hardcoded link
+        twitter: 'https://twitter.com/jycouet',
 
-    //     // reference to link with params!
-    //     twitter_post: 'https://twitter.com/[name]/status/[id]',
+        // reference to link with params!
+        twitter_post: 'https://twitter.com/[name]/status/[id]',
 
-    //     // reference to link with params & search params!
-    //     gravatar: {
-    //       href: 'https://www.gravatar.com/avatar/[str]',
-    //       params: {
-    //         str: { type: 'string' },
-    //       },
-    //       explicit_search_params: {
-    //         s: { type: 'number', default: 75 },
-    //         d: { type: '"retro" | "identicon"', default: '"identicon"' },
-    //       },
-    //     },
-    //   },
+        // reference to link with params & search params!
+        gravatar: {
+          href: 'https://www.gravatar.com/avatar/[str]',
+          params: {
+            str: { type: 'string' },
+          },
+          explicit_search_params: {
+            s: { type: 'number', default: 75 },
+            d: { type: '"retro" | "identicon"', default: '"identicon"' },
+          },
+        },
+      },
 
-    //   override_params: {
-    //     lang: { type: "'fr' | 'en' | 'hu' | 'at' | string" },
-    //   },
-    // }),
+      override_params: {
+        lang: { type: "'fr' | 'en' | 'hu' | 'at' | string" },
+      },
+    }),
   ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
