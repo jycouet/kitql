e899075d164f923e80682bf21f8c99c3ba6d7c88
jycouet <jycouet@gmail.com>
2023-12-12 18:23:28 +0100

    fix xif

--- packages/create-kitql/templates/kit-routes/.eslintrc.cjs
+++ packages/create-kitql/templates/kit-routes/.eslintrc.cjs
@@ -7,6 +7,9 @@ module.exports = {
 		'plugin:svelte/recommended',
 		'prettier'
 	],
+	rules: {
+		'@typescript-eslint/no-explicit-any': 'off'
+	},
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	parserOptions: {
