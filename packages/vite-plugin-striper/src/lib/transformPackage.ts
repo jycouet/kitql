b9edcafbec9b797060831b6cf020af98e8bdbd76
jycouet <jycouet@gmail.com>
2023-12-14 21:45:10 +0100

    :tada: NEW: just remove the import (#459)

---
+++ packages/vite-plugin-striper/src/lib/transformPackage.ts
@@ -0,0 +1,37 @@
+import { parse } from '@babel/parser'
+import * as recast from 'recast'
+import { prettyPrint } from 'recast'
+
+const { visit } = recast.types
+
+type Statement = recast.types.namedTypes.Statement
+
+export const removePackages = async (code: string, packages_to_strip: string[]) => {
+  try {
+    const ast = parse(code, {
+      plugins: ['typescript', 'decorators-legacy', 'importAssertions'],
+      sourceType: 'module',
+    })
+
+    const packages_striped: string[] = []
+
+    // Code to remove imports
+    visit(ast, {
+      visitImportDeclaration(path) {
+        const packageName = path.node.source.value
+        if (packages_to_strip.includes(String(packageName))) {
+          path.prune()
+          packages_striped.push(String(packageName))
+        }
+        return false
+      },
+    })
+
+    return {
+      code: recast.print(ast).code,
+      info: packages_striped.map(pkg => `Striped: '${pkg}'`),
+    }
+  } catch (error) {
+    return { code, info: [] }
+  }
+}
