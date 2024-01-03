7d1f9986a1e786f72f29c955c7e01d55bc2a2d43
Andreas Fehn <fehnomenal@fehn.systems>
2024-01-03 14:33:43 +0100

    Generate routes during build (#588)
    * call post update sync
    
    * generate routes during build and dev
    
    * changeset
    
    * await child process close

--- packages/vite-plugin-kit-routes/src/lib/plugin.ts
+++ packages/vite-plugin-kit-routes/src/lib/plugin.ts
@@ -775,7 +775,7 @@ const arrayToRecord = (arr?: string[]) => {
   return `: Record<string, never>`
 }
 
-export const run = (atStart: boolean, o?: Options) => {
+export const run = async (atStart: boolean, o?: Options) => {
   const options = getDefaultOption(o)
 
   const files = getFilesUnder(routes_path())
@@ -960,10 +960,14 @@ ${objTypes
         })
       }
 
+      const exitPromise = new Promise<void>(resolve => {
+        child.on('close', () => resolve())
+      })
+
+      await exitPromise
+
       if (shouldLog('update', options)) {
-        child.on('close', () => {
-          theEnd(atStart, result, objTypes, options)
-        })
+        theEnd(atStart, result, objTypes, options)
       }
     } else {
       theEnd(atStart, result, objTypes, options)
@@ -1069,8 +1073,8 @@ export function kitRoutes<T extends ExtendTypes = ExtendTypes>(options?: Options
     // Run the thing at startup
     {
       name: 'kit-routes',
-      configureServer() {
-        run(true, options)
+      async buildStart() {
+        await run(true, options)
       },
     },
 
@@ -1080,8 +1084,8 @@ export function kitRoutes<T extends ExtendTypes = ExtendTypes>(options?: Options
         name: 'kit-routes-watch',
         logs: [],
         watch: ['**/+page.svelte', '**/+page.server.ts', '**/+server.ts'],
-        run: () => {
-          run(false, options)
+        run: async () => {
+          await run(false, options)
         },
       },
     ]),
