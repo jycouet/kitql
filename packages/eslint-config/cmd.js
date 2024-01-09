6ecd3f388ece9523166c1f0a98e0b1acfdf658dd
jycouet <jycouet@gmail.com>
2024-01-09 19:53:50 +0100

    cleanup

--- packages/eslint-config/cmd.js
+++ packages/eslint-config/cmd.js
@@ -57,8 +57,6 @@ let result_prettier = spawn(cmdPrettier, {
 if (!format) {
   const logPrettier = new Log('kitql-lint prettier')
   result_prettier.stdout.on('data', data => {
-    console.log(`coucou`)
-
     logPrettier.error(
       data
         .toString()
