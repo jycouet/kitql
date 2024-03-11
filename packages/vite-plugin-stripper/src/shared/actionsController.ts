7a72809106b35bc2fe062adccf55b513ef0f2ad3
jycouet <jycouet@gmail.com>
2024-03-11 21:59:40 +0100

    tmp test strip

--- packages/vite-plugin-stripper/src/shared/actionsController.ts
+++ packages/vite-plugin-stripper/src/shared/actionsController.ts
@@ -1,3 +1,5 @@
+import { Bcrypt } from 'oslo/password'
+
 import { BackendMethod, remult, type Allowed } from 'remult'
 
 import { AUTH_SECRET } from '$env/static/private'
@@ -8,6 +10,9 @@ export class ActionsController {
     allowed: () => remult.user === undefined,
   })
   static async read(info: Allowed) {
+    const b = new Bcrypt()
+    b.hash('1234')
+
     console.info('AUTH_SECRET', AUTH_SECRET)
     return AUTH_SECRET + ' ' + info
   }
