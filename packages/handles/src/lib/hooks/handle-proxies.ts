a3cfde51d2ef4cf7c5a7297646bdd3df2a2bc804
Francis Nimick <fnimick@gmail.com>
2024-08-19 18:29:53 -0400

    :sparkles: NEW: requestHook option for handleProxies

--- packages/handles/src/lib/hooks/handle-proxies.ts
+++ packages/handles/src/lib/hooks/handle-proxies.ts
@@ -1,4 +1,4 @@
-import { error, type Handle } from '@sveltejs/kit'
+import { error, type Handle, type MaybePromise, type RequestEvent } from '@sveltejs/kit'
 
 import { Log } from '@kitql/helpers'
 
@@ -8,16 +8,66 @@ const log = new Log('handleProxies')
 
 export interface ProxyOptions {
   to: string
+  requestHook?: (event: RequestEvent) => MaybePromise<Request>
 }
 
 /**
- * Creates a handler which, for requests matching a given path prefix in the given options,
- * proxies the request to the `to` URL in the corresponding {@link ProxyOptions}. Any path elements
- * after the matching prefix are preserved, e.g. a request to `/from/some/other/path` will be
- * proxied to `to/some/other/path`. The request method, body, and headers are preserved, with the
- * exception of the `Host` header which is set to the proxy target hostname.
+ * Fetches a request, optionally using a proxied URL. If a proxied URL is provided, the request is
+ * made to that URL with the same method, body, and headers as the original request, with the `host`
+ * header set to the hostname of the target proxied URL. If no proxied URL is provided, the request
+ * is made to the original request URL.
+ *
+ * If the fetch fails, the error is logged and rethrown.
+ */
+function fetchMaybeProxiedRequest(
+  fetch: RequestEvent['fetch'],
+  request: Request,
+  proxiedUrl?: string,
+): Promise<Response> {
+  const url = proxiedUrl ?? request.url
+  const requestHeaders = new Headers(request.headers)
+  if (proxiedUrl != null) {
+    requestHeaders.set('host', new URL(proxiedUrl).hostname)
+  }
+  return fetch(url, {
+    body: request.body,
+    method: request.method,
+    headers: requestHeaders,
+    // typescript does not yet support the `duplex` property of `RequestInit`
+    // see: https://github.com/microsoft/TypeScript-DOM-lib-generator/issues/1483
+    // @ts-expect-error
+    duplex: 'half',
+  }).catch((err) => {
+    console.error(err)
+    log.error('handleProxies ERROR')
+    throw err
+  })
+}
+
+/**
+ * Creates a handler which, for requests matching a given path prefix in the given options, proxies
+ * the request to the `to` URL in the corresponding {@link ProxyOptions}. Any path elements after
+ * the matching prefix are preserved, e.g. a request to `/from/some/other/path` will be proxied to
+ * `to/some/other/path`. The request method, body, and headers are preserved, with the exception of
+ * the `Host` header which is set to the proxy target hostname.
+ *
+ * If a `requestHook` is defined, it is called with the original request event before the returned
+ * request is proxied. This allows for modifying the request before it is sent, e.g. to add or
+ * remove authentication headers, api keys, etc. If the returned request is to a URL with the same
+ * origin as the initial request and a path that also starts with the matched path prefix, that
+ * request path is proxied; otherwise, the returned request path is not modified before it is
+ * fetched. If the `requestHook` function throws, the request is not proxied and a response
+ * corresponding to the thrown object (matching Sveltekit endpoint behavior) is returned to the
+ * client instead.
  *
  * If multiple options entries would match a request, the first matching entry is used.
+ *
+ * For requests matching a path prefix in options that do not have an `Origin` header or that have
+ * an `Origin` header not matching the request's origin, a 403 Forbidden response is returned. This
+ * prevents use of the proxy by other browser applications, but (IMPORTANT) does not prevent abuse
+ * of the proxy by applications that can set the `Origin` header manually to match. To prevent this,
+ * you can require user authentication and authorization before allowing proxying, validating an
+ * incoming request either in a preceding hook or directly in the `requestHook`.
  */
 export function handleProxies(options: OptionsByStringPath<ProxyOptions>): Handle {
   return async ({ event, resolve }) => {
@@ -26,35 +76,32 @@ export function handleProxies(options: OptionsByStringPath<ProxyOptions>): Handl
       return resolve(event)
     }
 
-    const [from, { to }] = matchingPathAndOptions
+    const [from, { to, requestHook }] = matchingPathAndOptions
     const origin = event.request.headers.get('Origin')
     // reject requests that don't come from the webapp, to avoid your proxy being abused.
     if (!origin || new URL(origin).origin !== event.url.origin) {
       error(403, 'Forbidden')
     }
 
-    // strip "from" from the request path
-    const strippedPath = event.url.pathname.substring(from.length)
-    // build the new URL and request
-    const urlPath = `${to}${strippedPath}${event.url.search}`
-    const proxiedUrl = new URL(urlPath)
-    const requestHeaders = new Headers(event.request.headers)
-    requestHeaders.set('host', event.url.hostname)
+    let request = event.request
+    // if a requestHook is defined, call it with the original request event. any errors thrown by
+    // `requestHook` will be caught by sveltekit and handled as though they were thrown by an
+    // endpoint
+    if (requestHook != null) {
+      request = await requestHook(event)
+    }
+    const url = new URL(request.url)
+    // If the requestHook returns a request with a different origin or a path not from this
+    // working prefix, perform the request as-is
+    if (url.origin !== event.url.origin || !url.pathname.startsWith(from)) {
+      return fetchMaybeProxiedRequest(event.fetch, request)
+    }
 
-    return event
-      .fetch(proxiedUrl.toString(), {
-        body: event.request.body,
-        method: event.request.method,
-        headers: requestHeaders,
-        // typescript does not yet support the `duplex` property of `RequestInit`
-        // see: https://github.com/microsoft/TypeScript-DOM-lib-generator/issues/1483
-        // @ts-expect-error
-        duplex: 'half',
-      })
-      .catch((err) => {
-        console.error(err)
-        log.error('handleProxies ERROR')
-        throw err
-      })
+    // strip "from" from the request path
+    const strippedPath = url.pathname.substring(from.length)
+    // build the new URL and request
+    const urlPath = `${to}${strippedPath}${url.search}`
+    const proxiedUrl = new URL(urlPath).toString()
+    return fetchMaybeProxiedRequest(event.fetch, request, proxiedUrl)
   }
 }
---
+++ packages/handles/tests/handle-proxies-request-hooks.e2e.ts
@@ -0,0 +1,211 @@
+import { expect, test } from '@playwright/test'
+
+import { assertDefined } from './utils.js'
+
+// NOTE: all these tests rely on the behavior of the httpbin.org service
+
+test('default behavior: forbids requests with incorrect origin', async ({ request }) => {
+  const response = await request.get('/proxy-advanced/status/200', {
+    headers: {
+      origin: 'http://example.com',
+    },
+  })
+  expect(response.status()).toBe(403)
+})
+
+test('default behavior: forbids requests with no origin specified', async ({ request }) => {
+  const response = await request.get('/proxy-advanced/status/200')
+  expect(response.status()).toBe(403)
+})
+
+test.describe('default behavior: requests with correct origin', () => {
+  test('proxies requests as expected to specified endpoint', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.get('/proxy-advanced/status/200', {
+      headers: {
+        origin: baseURL,
+      },
+    })
+    expect(response.status()).toBe(200)
+  })
+  test('sets the host header to the target url host', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.get('/proxy-advanced/headers', {
+      headers: {
+        origin: baseURL,
+      },
+    })
+    expect(response.status()).toBe(200)
+    const body = await response.json()
+    expect(body.headers['Host']).toBe('eu.httpbin.org')
+  })
+  test('proxies the query parameters', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.get('/proxy-advanced/get?param=value', {
+      headers: {
+        origin: baseURL,
+      },
+    })
+    expect(response.status()).toBe(200)
+    const body = await response.json()
+    expect(body.args).toStrictEqual({ param: 'value' })
+  })
+  test('proxies the application/json body of a post request', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.post('/proxy-advanced/post', {
+      headers: {
+        origin: baseURL,
+      },
+      data: { foo: 'bar' },
+    })
+    expect(response.status()).toBe(200)
+    const body = await response.json()
+    expect(body.headers['Content-Type']).toBe('application/json')
+    expect(body.json).toStrictEqual({ foo: 'bar' })
+  })
+  test('proxies the application/x-www-form-urlencoded body of a post request', async ({
+    request,
+    baseURL,
+  }) => {
+    assertDefined(baseURL)
+    const response = await request.post('/proxy-advanced/post', {
+      headers: {
+        origin: baseURL,
+      },
+      form: { foo: 'bar' },
+    })
+    expect(response.status()).toBe(200)
+    const body = await response.json()
+    const contentTypeHeader = body.headers['Content-Type']
+    assertDefined(contentTypeHeader)
+    expect(contentTypeHeader.startsWith('application/x-www-form-urlencoded')).toBe(true)
+    expect(body.form).toStrictEqual({ foo: 'bar' })
+  })
+  test('proxies the multipart/form-data body of a post request', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.post('/proxy-advanced/post', {
+      headers: {
+        origin: baseURL,
+      },
+      multipart: {
+        foo: 'bar',
+        file_input: {
+          name: 'test.txt',
+          mimeType: 'text/plain',
+          buffer: Buffer.from('file content'),
+        },
+      },
+    })
+    expect(response.status()).toBe(200)
+    const body = await response.json()
+    const contentTypeHeader = body.headers['Content-Type']
+    assertDefined(contentTypeHeader)
+    expect(contentTypeHeader.startsWith('multipart/form-data')).toBe(true)
+    expect(body.form).toStrictEqual({ foo: 'bar' })
+    expect(body.files).toStrictEqual({ file_input: 'file content' })
+  })
+  test('proxies the application/json body of a delete request', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.delete('/proxy-advanced/delete', {
+      headers: {
+        origin: baseURL,
+      },
+      data: { foo: 'bar' },
+    })
+    expect(response.status()).toBe(200)
+    const body = await response.json()
+    expect(body.headers['Content-Type']).toBe('application/json')
+    expect(body.json).toStrictEqual({ foo: 'bar' })
+  })
+  test('proxies the application/json body of a put request', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.put('/proxy-advanced/put', {
+      headers: {
+        origin: baseURL,
+      },
+      data: { foo: 'bar' },
+    })
+    expect(response.status()).toBe(200)
+    const body = await response.json()
+    expect(body.headers['Content-Type']).toBe('application/json')
+    expect(body.json).toStrictEqual({ foo: 'bar' })
+  })
+  test('proxies the application/json body of a patch request', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.patch('/proxy-advanced/patch', {
+      headers: {
+        origin: baseURL,
+      },
+      data: { foo: 'bar' },
+    })
+    expect(response.status()).toBe(200)
+    const body = await response.json()
+    expect(body.headers['Content-Type']).toBe('application/json')
+    expect(body.json).toStrictEqual({ foo: 'bar' })
+  })
+})
+
+test.describe('custom requestHook behavior', () => {
+  test('allows redirect()', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.get('/proxy-advanced/test-redirect', {
+      headers: { origin: baseURL },
+      maxRedirects: 0,
+    })
+    expect(response.status()).toBe(302)
+    expect(response.headers().location).toBe('https://kitql.dev/')
+  })
+  test('allows error()', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.get('/proxy-advanced/test-error', {
+      headers: { origin: baseURL },
+    })
+    expect(response.status()).toBe(418)
+    const body = await response.json()
+    expect(body).toStrictEqual({
+      message: 'Custom teapot-related error message',
+      isTeapot: true,
+    })
+  })
+  test('proxies requests with new paths matching the prefix', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.get('/proxy-advanced/test-rewrite-path-same-prefix', {
+      headers: { origin: baseURL },
+    })
+    expect(response.status()).toBe(208)
+  })
+  test('allows and does not proxy new paths that do not match the prefix', async ({
+    request,
+    baseURL,
+  }) => {
+    assertDefined(baseURL)
+    const response = await request.get('/proxy-advanced/test-rewrite-path-no-matching-prefix', {
+      headers: { origin: baseURL },
+    })
+    expect(response.status()).toBe(200)
+    // proxied to cors custom header testing endpoint
+    expect(response.headers()['x-custom-header']).toBe('custom get value')
+  })
+  test('allows using locals', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.get('/proxy-advanced/test-using-locals', {
+      headers: { origin: baseURL },
+    })
+    expect(response.status()).toBe(200)
+    const body = await response.json()
+    expect(body.args).toStrictEqual({ test_data: 'test-locals-data' })
+  })
+  test('allows manipulation of request headers before proxying', async ({ request, baseURL }) => {
+    assertDefined(baseURL)
+    const response = await request.get('/proxy-advanced/test-manipulating-headers', {
+      headers: {
+        origin: baseURL,
+        Authorization: 'Bearer token',
+      },
+    })
+    expect(response.status()).toBe(200)
+    const body = await response.json()
+    expect(body.headers['Authorization']).toBeUndefined()
+    expect(body.headers['X-Api-Key']).toBe('sample-api-key')
+  })
+})
