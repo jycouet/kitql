115fe7ce26c755812bfba8f0aef56d946650df74
Francis Nimick <fnimick@gmail.com>
2024-08-19 18:27:40 -0400

    :white_check_mark: UPDATE: add explicit non-OPTIONS header preservation tests for cors utilities

--- packages/handles/src/routes/api/cors-handler/default-options/with-response-headers/+server.ts
+++ packages/handles/src/routes/api/cors-handler/default-options/with-response-headers/+server.ts
@@ -3,10 +3,10 @@ import { json } from '@sveltejs/kit'
 export async function OPTIONS() {
   return new Response(null, {
     status: 204,
-    headers: { 'X-Custom-Header': 'custom value', 'Access-Control-Max-Age': '42' },
+    headers: { 'X-Custom-Header': 'custom options value', 'Access-Control-Max-Age': '42' },
   })
 }
 
 export async function GET() {
-  return json({ message: 'Success message' }, { headers: { 'X-Custom-Header': 'custom value' } })
+  return json({ message: 'Success message' }, { headers: { 'X-Custom-Header': 'custom get value' } })
 }
--- packages/handles/src/routes/api/cors-wrapper/default-options/with-response-headers/+server.ts
+++ packages/handles/src/routes/api/cors-wrapper/default-options/with-response-headers/+server.ts
@@ -8,10 +8,10 @@ export const OPTIONS = wrapper(
   () =>
     new Response(null, {
       status: 204,
-      headers: { 'X-Custom-Header': 'custom value', 'Access-Control-Max-Age': '42' },
+      headers: { 'X-Custom-Header': 'custom options value', 'Access-Control-Max-Age': '42' },
     }),
 )
 
 export const GET = wrapper(() =>
-  json({ message: 'Success message' }, { headers: { 'X-Custom-Header': 'custom value' } }),
+  json({ message: 'Success message' }, { headers: { 'X-Custom-Header': 'custom get value' } }),
 )
