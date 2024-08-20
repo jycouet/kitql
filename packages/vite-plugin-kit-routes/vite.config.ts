b8f2209f03e6fc3782d6d5dc6dc44e2be4a0ad67
jycouet <jycouet@gmail.com>
2024-08-20 18:34:37 +0200

    tentative

--- packages/vite-plugin-kit-routes/vite.config.ts
+++ packages/vite-plugin-kit-routes/vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 import type { KIT_ROUTES } from '$lib/ROUTES.js'
 
