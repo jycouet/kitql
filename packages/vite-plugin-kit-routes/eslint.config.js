0f20d145b253ea097f0a5d7cbc8bd60f4f724e86
jycouet <jycouet@gmail.com>
2024-09-11 20:22:41 +0200

    let's see :(

--- packages/vite-plugin-kit-routes/eslint.config.js
+++ packages/vite-plugin-kit-routes/eslint.config.js
@@ -2,11 +2,11 @@ import kitql from '@kitql/eslint-config'
 
 /** @type { import("eslint").Linter.Config } */
 export default [
-  ...kitql,
   {
     rules: {
       '@typescript-eslint/no-unsafe-function-type': 'off',
       'no-unused-disable': 'off',
     },
   },
+  ...kitql,
 ]
