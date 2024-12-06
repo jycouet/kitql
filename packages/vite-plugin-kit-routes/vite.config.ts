ceea2b22d2e54fd181901b2be17327a3d7070091
jycouet <jycouet@gmail.com>
2024-12-06 20:13:24 +0100

    fixing types

--- packages/vite-plugin-kit-routes/vite.config.ts
+++ packages/vite-plugin-kit-routes/vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 
 import type { KIT_ROUTES } from '$lib/ROUTES.js'
 
@@ -119,7 +119,4 @@ export default defineConfig({
       },
     }),
   ],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
-  },
 })
