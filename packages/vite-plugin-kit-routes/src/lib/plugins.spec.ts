f86e8b740d1d3809e8b233765e4c82eeaf6d56af
jycouet <jycouet@gmail.com>
2025-01-12 15:41:53 +0100

    log content

--- packages/vite-plugin-kit-routes/src/lib/plugins.spec.ts
+++ packages/vite-plugin-kit-routes/src/lib/plugins.spec.ts
@@ -841,6 +841,8 @@ describe('run()', async () => {
     })
 
     const content = read(generated_file_path) ?? ''
+    console.log(`content`, content)
+
     expect(content.includes('import { base } from')).toBe(true)
     expect(content.includes('$app/paths')).toBe(true)
     expect(content.includes('${base}')).toBe(true)
