f1f278c410afaf8d7e906d02281912459567dc80
jycouet <jycouet@gmail.com>
2025-03-07 22:53:56 +0100

    :sparkles: NEW: pnpmCatalogs with config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,17 @@
 import { kitql } from './packages/eslint-config/eslint.config.js'
 
-/** @type { import("eslint").Linter.Config } */
+/** @type { import("eslint").Linter.Config[] } */
 export default [
-	...kitql({ pnpmCatalogs: true }),
+	...kitql({
+		// pnpmCatalogs: {
+		// 	enable: true,
+		// 	files: ['package.json', '**/*.package.json'],
+		// 	rules: {
+		// 		'pnpm-catalogs/enforce-catalog': 'error',
+		// 		'pnpm-catalogs/valid-catalog': 'error',
+		// 	},
+		// },
+	}),
 	{
 		name: 'APP:ignores',
 		ignores: ['**/*.svelte.ts'],
