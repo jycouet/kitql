63674a8e01792a17a5a878d9786beab06a8af9bc
jyc.dev <jycouet@gmail.com>
2025-04-23 17:21:39 +0200

    :construction: NEW: kit routes cli v3 (#984)

---
+++ .changeset/five-cases-stop.md
@@ -0,0 +1,5 @@
+---
+'vite-plugin-kit-routes': patch
+---
+
+Read well the config for cli
--- packages/vite-plugin-kit-routes/src/lib/ast.ts
+++ packages/vite-plugin-kit-routes/src/lib/ast.ts
@@ -115,6 +115,150 @@ export const getActionsOfServerPages = (pathFile: string) => {
 	return { actions, withLoad }
 }
 
+export function evaluateNode(node: any): any {
+	if (node.type === 'Literal') {
+		return node.value
+	} else if (node.type === 'StringLiteral') {
+		return node.value
+	} else if (node.type === 'BooleanLiteral') {
+		return node.value
+	} else if (node.type === 'NumericLiteral') {
+		return node.value
+	} else if (node.type === 'ObjectExpression') {
+		const result: any = {}
+		node.properties.forEach((prop: any) => {
+			if (prop.type === 'ObjectProperty') {
+				const key =
+					prop.key.type === 'Identifier'
+						? prop.key.name
+						: prop.key.type === 'StringLiteral'
+							? prop.key.value
+							: prop.key.type === 'NumericLiteral'
+								? prop.key.value
+								: prop.key.value
+				result[key] = evaluateNode(prop.value)
+			}
+		})
+		return result
+	} else if (node.type === 'ArrayExpression') {
+		return node.elements.map((element: any) => evaluateNode(element))
+	} else if (node.type === 'Identifier') {
+		return node.name
+	} else if (node.type === 'TemplateLiteral') {
+		return node.quasis.map((quasi: any) => quasi.value.raw).join('')
+	} else if (node.type === 'ArrowFunctionExpression' || node.type === 'FunctionExpression') {
+		// Return the function as is for now
+		return node
+	} else if (node.type === 'CallExpression') {
+		return {
+			callee: evaluateNode(node.callee),
+			arguments: node.arguments.map((arg: any) => evaluateNode(arg)),
+		}
+	} else if (node.type === 'MemberExpression') {
+		return {
+			object: evaluateNode(node.object),
+			property: evaluateNode(node.property),
+			computed: node.computed,
+		}
+	} else if (node.type === 'BinaryExpression') {
+		return {
+			operator: node.operator,
+			left: evaluateNode(node.left),
+			right: evaluateNode(node.right),
+		}
+	} else if (node.type === 'ConditionalExpression') {
+		return {
+			test: evaluateNode(node.test),
+			consequent: evaluateNode(node.consequent),
+			alternate: evaluateNode(node.alternate),
+		}
+	} else if (node.type === 'LogicalExpression') {
+		return {
+			operator: node.operator,
+			left: evaluateNode(node.left),
+			right: evaluateNode(node.right),
+		}
+	} else if (node.type === 'UnaryExpression') {
+		return {
+			operator: node.operator,
+			argument: evaluateNode(node.argument),
+		}
+	} else if (node.type === 'UpdateExpression') {
+		return {
+			operator: node.operator,
+			argument: evaluateNode(node.argument),
+			prefix: node.prefix,
+		}
+	} else if (node.type === 'AssignmentExpression') {
+		return {
+			operator: node.operator,
+			left: evaluateNode(node.left),
+			right: evaluateNode(node.right),
+		}
+	} else if (node.type === 'SequenceExpression') {
+		return node.expressions.map((expr: any) => evaluateNode(expr))
+	} else if (node.type === 'SpreadElement') {
+		return {
+			...evaluateNode(node.argument),
+		}
+	} else if (node.type === 'NewExpression') {
+		return {
+			callee: evaluateNode(node.callee),
+			arguments: node.arguments.map((arg: any) => evaluateNode(arg)),
+		}
+	} else if (node.type === 'YieldExpression') {
+		return {
+			argument: node.argument ? evaluateNode(node.argument) : undefined,
+			delegate: node.delegate,
+		}
+	} else if (node.type === 'AwaitExpression') {
+		return {
+			argument: evaluateNode(node.argument),
+		}
+	} else if (node.type === 'TaggedTemplateExpression') {
+		return {
+			tag: evaluateNode(node.tag),
+			quasi: evaluateNode(node.quasi),
+		}
+	} else if (node.type === 'ClassExpression') {
+		return {
+			id: node.id ? evaluateNode(node.id) : null,
+			superClass: node.superClass ? evaluateNode(node.superClass) : null,
+			body: evaluateNode(node.body),
+		}
+	} else if (node.type === 'ClassBody') {
+		return node.body.map((method: any) => evaluateNode(method))
+	} else if (node.type === 'MethodDefinition') {
+		return {
+			key: evaluateNode(node.key),
+			value: evaluateNode(node.value),
+			kind: node.kind,
+			static: node.static,
+			computed: node.computed,
+		}
+	} else if (node.type === 'ClassDeclaration') {
+		return {
+			id: evaluateNode(node.id),
+			superClass: node.superClass ? evaluateNode(node.superClass) : null,
+			body: evaluateNode(node.body),
+		}
+	} else if (node.type === 'MetaProperty') {
+		return {
+			meta: evaluateNode(node.meta),
+			property: evaluateNode(node.property),
+		}
+	} else if (node.type === 'Super') {
+		return 'super'
+	} else if (node.type === 'Import') {
+		return 'import'
+	} else if (node.type === 'ThisExpression') {
+		return 'this'
+	} else {
+		log.error('Unknown node type: ' + node.type + ' (fallback to node.value)')
+		return node.value
+	}
+}
+
 export const getExportsFromFile = (code: string, exportName?: string) => {
 	try {
 		const codeParsed = parse(code)
