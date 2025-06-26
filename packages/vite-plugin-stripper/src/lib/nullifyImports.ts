48d2067f5d438df528210f9806947fecaf107bd4
jycouet <jycouet@gmail.com>
2025-06-27 00:06:18 +0200

    starting

--- packages/vite-plugin-stripper/src/lib/nullifyImports.ts
+++ packages/vite-plugin-stripper/src/lib/nullifyImports.ts
@@ -1,4 +1,4 @@
-import { builders, parse, visit } from '@kitql/internals'
+import { parse, walk } from '@kitql/internals'
 import type { ParseResult } from '@kitql/internals'
 
 export const nullifyImports = async (
@@ -10,32 +10,34 @@ export const nullifyImports = async (
 
 		const packages_striped: string[] = []
 
-		visit(ast, {
-			visitImportDeclaration(path: any) {
-				const packageName = path.node.source.value
-				if (packages_to_strip.includes(String(packageName))) {
-					const specifiers = path.node.specifiers!
-					const replacementNodes = specifiers
-						.map((specifier: any) => {
-							if (specifier.type === 'ImportSpecifier') {
-								return builders.variableDeclaration('let', [
-									builders.variableDeclarator(
-										builders.identifier(String(specifier.imported.name)),
-										builders.literal(null),
-									),
-								])
-							}
-						})
-						.filter(Boolean) // Remove undefined values
+		walk(ast.program, {
+			enter(node, parent) {
+				if (node.type === 'ImportDeclaration') {
+					const packageName = node.source.value
+					if (packages_to_strip.includes(String(packageName))) {
+						const specifiers = node.specifiers!
+						const replacementNodes = specifiers
+							.map((specifier: any) => {
+								if (specifier.type === 'ImportSpecifier') {
+									// return builders.variableDeclaration('let', [
+									// 	builders.variableDeclarator(
+									// 		builders.identifier(String(specifier.imported.name)),
+									// 		builders.literal(null),
+									// 	),
+									// ])
+								}
+							})
+							.filter(Boolean) // Remove undefined values
 
-					if (replacementNodes.length > 0) {
-						path.replace(...replacementNodes)
-						packages_striped.push(String(packageName))
-					} else {
-						path.prune()
+						if (replacementNodes.length > 0) {
+							// path.replace(...replacementNodes)
+							packages_striped.push(String(packageName))
+						} else {
+							// path.prune()
+						}
 					}
+					return false
 				}
-				return false
 			},
 		})
 
