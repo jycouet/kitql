2f055bff1f1709b6b09797be240317db4ceb1702
jycouet <jycouet@gmail.com>
2025-10-28 16:31:20 +0100

    ast seems ok

--- packages/internals/src/lib/ast/ast.ts
+++ packages/internals/src/lib/ast/ast.ts
@@ -1,33 +1,41 @@
-import babel from '@babel/parser'
-import * as recast from 'recast'
+import { print as esrap_print } from 'esrap'
+import ts from 'esrap/languages/ts'
+import { parseSync } from 'oxc-parser'
+import type { ParseResult } from 'oxc-parser'
+import { walk } from 'oxc-walker'
 
-export type Statement = recast.types.namedTypes.Statement
-export type ParseResult = babel.ParseResult
-export const { visit, builders } = recast.types
+import { oxcCommentsToEsrapComments } from './oxcCommentsToEsrapComments.js'
 
-/** @deprecated Use `parse` instead */
-export function parseTs(source: string | null) {
-	const parsed = babel.parse(source ?? '', {
-		sourceType: 'module',
-		plugins: ['typescript', 'importAssertions', 'decorators-legacy'],
-	})
+export { walk }
+export type KitQLParseResult = ParseResult & { code: string }
+export type ParseOptions = { filename?: string; lang?: 'ts' }
 
-	return parsed.program as ParseResult['program']
-}
-
-export function parse(code_ast: string | ParseResult) {
+export function parse(code_ast: string | null | KitQLParseResult, options?: ParseOptions) {
 	if (code_ast === null || typeof code_ast === 'string') {
-		const parsed = babel.parse(code_ast ?? '', {
+		const parsed = parseSync(options?.filename ?? 'default.ts', code_ast ?? '', {
 			sourceType: 'module',
-			plugins: ['typescript', 'importAssertions', 'decorators-legacy'],
+			lang: options?.lang ?? 'ts',
 		})
 
-		return parsed
+		return { ...parsed, code: code_ast ?? '' } as KitQLParseResult
 	}
 
 	return code_ast
 }
 
-export function print(node: recast.types.namedTypes.Program, options?: recast.Options) {
-	return recast.prettyPrint(node, options)
+export function print(ast: KitQLParseResult, options?: {}) {
+	const { ast: ast_with_comments, comments } = oxcCommentsToEsrapComments(
+		ast.code,
+		// @ts-expect-error
+		ast.program,
+		ast.comments,
+	)
+
+	try {
+		return esrap_print(ast_with_comments, ts({ comments }))
+	} catch (error) {
+		console.dir(error, { depth: null })
+		// console.dir(ast.program, { depth: null })
+		throw error
+	}
 }
