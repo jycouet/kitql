bad891c938fbc79ef8567dc168369ef471403af9
jycouet <jycouet@gmail.com>
2025-10-28 16:58:33 +0100

    test https://github.com/sveltejs/esrap/pull/85 and the entity is fully there

--- packages/internals/src/lib/ast/ast.spec.ts
+++ packages/internals/src/lib/ast/ast.spec.ts
@@ -85,7 +85,31 @@ const b = "hello"
 			"import { BackendMethod, Entity, Fields, remult, type Allowed } from 'remult';
 			import { AUTH_SECRET } from '$env/static/private';
 
-			export class User {
+			export @Entity('users', {
+				allowApiCrud: true,
+
+				backendPrefilter: () => {
+					console.info('AUTH_SECRET_backendPrefilter', AUTH_SECRET);
+					console.info('backendPrefilter_top_secret');
+
+					return {};
+				},
+
+				backendPreprocessFilter: (f) => {
+					console.info('AUTH_SECRET_backendPreprocessFilter', AUTH_SECRET);
+					console.info('backendPreprocessFilter_top_secret');
+
+					return f;
+				},
+
+				sqlExpression: () => {
+					console.info('AUTH_SECRET_sqlExpression', AUTH_SECRET);
+					console.info('sqlExpression_top_secret');
+
+					return 'users';
+				}
+			})
+			class User {
 				@Fields.uuid()
 				id = '';
 
